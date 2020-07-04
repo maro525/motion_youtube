d70edf4720af44544ff90f8d52228208e0d33bde
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-07-04 15:57:29 +0900

    add embed page

---
+++ pages/embed.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container indexcontainer">
+    <HumanPose :videoId="videoId" />
+  </section>
+</template>
+
+<script>
+import HumanPose from "~/components/layouts/HumanPose.vue";
+
+export default {
+  data() {
+    return {
+      videoId: "wgZcoo-eQO0",
+    };
+  },
+  components: {
+    HumanPose
+  },
+};
+</script>
+
+<style>
+.centering {
+  margin: auto 10px;
+}
+.indexcontainer {
+  margin-bottom: 140px;
+}
+</style>
+
